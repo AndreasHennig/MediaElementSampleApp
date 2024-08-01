20ca38e1464f3fa3a706c7707b8106ecf4478ba5
Andreas Hennig <hennig@adesso-mobile.de>
2024-08-01 10:22:29 +0200

    initial commit

---
+++ src/MediaElementSampleApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaElementSampleApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/MediaElementSampleApp/Platforms/Android/MainActivity.cs
@@ -0,0 +1,13 @@
+﻿using Android.App;
+using Android.Content.PM;
+
+namespace MediaElementSampleApp;
+
+[Activity(Theme = "@style/Maui.SplashTheme",
+          MainLauncher = true, 
+          LaunchMode = LaunchMode.SingleTask,
+          ResizeableActivity = true,
+          ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
